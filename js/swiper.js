c6ee7b5752b23097dd2a3dc80472b1705b5892a8
Junaid <sj.shaikhjunaid@gmail.com>
2022-02-18 17:11:40 +0530

    implement carousel functionality

--- js/swiper.js
+++ js/swiper.js
@@ -1,16 +1,40 @@
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
+const preBtn = document.querySelector(".swiper-button-prev");
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
+const nextBtn = document.querySelector(".swiper-button-next");
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+const swiperWrapper = document.querySelector(".swiper-wrapper");
+
+let active = 0;
+
+preBtn.style.display = "none";
+
+const showNextSlide = () => {
+  active++;
+
+  if (active > 0) {
+    preBtn.style.display = "block";
+  }
+
+  swiperWrapper.style.transform = `translateX(-${active * 100}%)`;
+
+  if (active >= swiperWrapper.children.length - 1) {
+    nextBtn.style.display = "none";
+  }
+};
+
+const showPrevSlide = () => {
+  active--;
+
+  if (active <= swiperWrapper.children.length - 1) {
+    nextBtn.style.display = "block";
+  }
+
+  swiperWrapper.style.transform = `translateX(-${active * 100}%)`;
+
+  if (active <= 0) {
+    preBtn.style.display = "none";
+  }
+};
+
+nextBtn.addEventListener("click", showNextSlide);
+preBtn.addEventListener("click", showPrevSlide);
